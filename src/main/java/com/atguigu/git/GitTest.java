45274f7179537263ad4fe17bb9731c5a2c34ed69
silence <792245022@qq.com>
2021-09-28 02:07:10 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author mwl_silence
+ * @create 2021-09-28 2:01
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
